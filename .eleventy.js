b98d943299a2b76dd7c584a0f9d89c1e4ef27927
Jacob Sherin <jacob@protoship.io>
2022-06-20 14:58:55 +0530

    copy CNAME to published directory

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,9 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  // Copy CNAME to the output
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
